5e915748dcd63955cd2ed1e165917c8127e8f304
dutayuekilan <xiangsh19725@126.com>
2021-04-26 23:24:11 +0800

    配置maven

---
+++ src/main/java/com/itcast/yuanyuan/oademo/App.java
@@ -0,0 +1,13 @@
+package com.itcast.yuanyuan.oademo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
